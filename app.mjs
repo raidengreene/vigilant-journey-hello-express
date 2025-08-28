701fd037558944b243ecc009a386f18b8ec61ac7
raidengreene <raideg002@gmail.com>
2025-08-28 13:16:26 -0500

    fixed webpage

--- app.mjs
+++ app.mjs
@@ -1,19 +1,21 @@
 import express from 'express'
-import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.use(express.static(__dirname + "public"))
+app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send("Hello Express from render.  <a href='/raiden'>raiden</a>")
 })
 app.get('/raiden', (req, res) => {
   //res.send("raiden <a href='/'>home</a>")
-  res.sendFile('raiden.html');
+  res.sendFile(join(__dirname, 'public', 'raiden.html'))
 })
 //endpoints...middleware...apis?
 //send an html file
